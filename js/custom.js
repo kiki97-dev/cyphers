2d4ab7076ed85dd23fd9bcd20a7e3ce8d94ab3a5
dltmd1502@naver.com <dltmd@1502@naver.com>
2022-11-04 02:52:40 +0900

    test1

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,13 @@
-$(document).ready(function () {
-    
-});
+fetch('https://api.neople.co.kr/cy/players?nickname=운동장&wordType=match&apikey=yq5RpAnjxLsLC5H2xdLNvFXfPaWdqwvo')
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error('네트워크 응답이 올바르지 않습니다.');
+        }
+        return response.blob();
+    })
+    .then((myBlob) => {
+        console.log(myBlob);
+    })
+    .catch((error) => {
+        console.error('fetch에 문제가 있었습니다.', error);
+    });
