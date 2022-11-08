f1501465c31c91abcb030092da9814025da937a7
dltmd1502@naver.com <dltmd@1502@naver.com>
2022-11-08 16:31:05 +0900

    테스트3

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,2 @@
-const test = await fetch('https://api.neople.co.kr/cy/players?nickname=운동장&wordType=<wordType>&apikey=yq5RpAnjxLsLC5H2xdLNvFXfPaWdqwvo').then(res => res.json());
+const test = fetch('https://api.neople.co.kr/cy/players?nickname=운동장&wordType=<wordType>&apikey=yq5RpAnjxLsLC5H2xdLNvFXfPaWdqwvo').then(res => res.json());
 console.log(test);
